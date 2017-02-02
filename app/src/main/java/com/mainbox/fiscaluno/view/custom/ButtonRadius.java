20ffa7bd2c40bc1a664765e960e1b297a362808d
kleber99 <kleber_nunes99@hotmail.com>
2017-02-02 19:12:52 -0200

    tela de pesquisa

---
+++ app/src/main/java/com/mainbox/fiscaluno/view/custom/ButtonRadius.java
@@ -0,0 +1,26 @@
+package com.mainbox.fiscaluno.view.custom;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+/**
+ * Created by Admin on 02/02/2017.
+ */
+
+public class ButtonRadius extends Button {
+
+    public ButtonRadius(Context context) {
+        super(context);
+        setBackground(getResources().getDrawable(R.drawable.buttonradius));
+        setTextColor(getResources().getColor(android.R.color.white));
+    }
+
+    public ButtonRadius(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setBackground(getResources().getDrawable(R.drawable.buttonradius));
+        setTextColor(getResources().getColor(android.R.color.white));
+    }
+
+}
