4b47d90ee33aefa85670b2fec49f93ea645f1b36
Wilder Pereira <wilder_roberto@hotmail.com>
2017-02-02 22:53:31 -0200

    melhorias layout

---
+++ app/src/main/java/com/mainbox/fiscaluno/view/instituicao/InstituicaoActivity.java
@@ -0,0 +1,15 @@
+package com.mainbox.fiscaluno.view.instituicao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mainbox.fiscaluno.R;
+
+public class InstituicaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instituicao);
+    }
+}
