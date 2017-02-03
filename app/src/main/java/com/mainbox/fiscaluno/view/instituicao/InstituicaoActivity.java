e855b02f9d190dcdcf9d0f56e4506eda1f74e937
Wilder Pereira <wilder_roberto@hotmail.com>
2017-02-02 23:23:14 -0200

    Tela instituicao

--- app/src/main/java/com/mainbox/fiscaluno/view/instituicao/InstituicaoActivity.java
+++ app/src/main/java/com/mainbox/fiscaluno/view/instituicao/InstituicaoActivity.java
@@ -2,14 +2,20 @@ package com.mainbox.fiscaluno.view.instituicao;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.mainbox.fiscaluno.R;
 
 public class InstituicaoActivity extends AppCompatActivity {
 
+    TextView nomeInstituicaoTv, mediaTv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instituicao);
+
+        nomeInstituicaoTv = (TextView) findViewById(R.id.nomeInstituicao);
+        mediaTv = (TextView) findViewById(R.id.media_tv);
     }
 }
